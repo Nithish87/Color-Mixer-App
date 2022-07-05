e037890d28a30d5d388025b0895766f5eb6a1dfd
Nithish <nithishprabhu123@gmail.com>
2022-07-05 23:08:37 +0530

    Added Main content

--- app/src/main/java/com/example/colormixerapp/Register.java
+++ app/src/main/java/com/example/colormixerapp/Register.java
@@ -1,4 +1,93 @@
 package com.example.colormixerapp;
 
-public class Register {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    EditText jFullName,jEmail,jPassword;
+    Button jRegisterButton;
+    TextView jLoginButton;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        jFullName=findViewById(R.id.fullName);
+        jEmail=findViewById(R.id.Email);
+        jPassword=findViewById(R.id.Password);
+        jRegisterButton=findViewById(R.id.registerButton);
+        jLoginButton=findViewById(R.id.alreadyRegistered);
+        fAuth=FirebaseAuth.getInstance();
+        progressBar=findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        jRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=jEmail.getText().toString().trim();
+                String password=jPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    jEmail.setError("Email is required!!!");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    jPassword.setError("Password is required!!!");
+                    return;
+                }
+
+                if(password.length()<6){
+                    jPassword.setError("Password must atleast 6 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //register user to firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this,"User Created Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(Register.this, "Error!!! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+
+                        }
+                    }
+                });
+            }
+        });
+
+        jLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+    }
 }
